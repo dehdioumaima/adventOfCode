d48647c742be01ad8b9099a880c7e406266d9f92
oumaima dehdi <oumaima.dehdi@acorel.nl>
2022-12-05 00:16:00 +0100

    advent4

---
+++ advent4/src/part2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class part2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader("/Users/oumaimaDehdi/dev/repos/adventOfCode/advent4/input/section-pairs.txt"));
+
+        int score =0;
+        String line = br.readLine();
+        while (line != null) {
+            String[] items = line.split(",");
+            String[] first = items[0].split("-");
+            String[] second = items[1].split("-");
+
+            int firstMin=Integer.parseInt(first[0]);
+            int firstMax=Integer.parseInt(first[1]);
+            int secondMin=Integer.parseInt(second[0]);
+            int secondMax=Integer.parseInt(second[1]);
+
+            if((secondMin <= firstMin && firstMin <= secondMax) ||
+                    (secondMin <= firstMax && firstMax <= secondMax) ||
+                    (firstMin <= secondMin && secondMin <= firstMax) ||
+                    (firstMin <= secondMax && secondMax <= firstMax))
+            {
+                score++;
+            }
+            line = br.readLine();
+        }
+
+        System.out.println("the score is: "+score);
+        br.close();
+    }
+}
